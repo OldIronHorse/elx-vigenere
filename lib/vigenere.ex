c3b14526a960e5d048c5fbff9422656d7b5858aa
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-06-23 13:34:19 +0100

    Initial empty mix project

---
+++ lib/vigenere.ex
@@ -0,0 +1,18 @@
+defmodule Vigenere do
+  @moduledoc """
+  Documentation for Vigenere.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vigenere.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vigenere_test.exs
@@ -0,0 +1,8 @@
+defmodule VigenereTest do
+  use ExUnit.Case
+  doctest Vigenere
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
